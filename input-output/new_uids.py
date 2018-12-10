36b9e538fa851c8d8442e164e38185ca6a0b1604
scaramallion <scaramallion@users.noreply.github.com>
2018-12-10 12:45:12 +1100

    Add more control over output, fix bug

--- input-output/new_uids.py
+++ input-output/new_uids.py
@@ -2,10 +2,10 @@
 """Recursively search a directory for DICOM datasets and generate new and
 consistent UIDs for the following top-level elements:
 
-* (0020,000D) Study Instance UID
+* (0020,000D) Study Instance UID (without --series)
 * (0020,000E) Series Instance UID
 * (0008,0018) SOP Instance UID
-* (0020,0052) Frame of Reference UID
+* (0020,0052) Frame of Reference UID (without --series or with --force-for)
 
 Possible uses:
 
@@ -17,10 +17,11 @@ from __future__ import print_function
 
 import argparse
 import os
+import shutil
 import sys
 
 from pydicom import dcmread
-from pydicom.uid import generate_uid, PYDICOM_ROOT_UID
+from pydicom.uid import generate_uid, PYDICOM_ROOT_UID, UID
 
 
 def setup_argparse():
@@ -35,8 +36,8 @@ def setup_argparse():
         description=(
             "Search a directory for DICOM datasets and generate new and "
             "consistent UIDs for (0020,000D) 'Study Instance UID', "
-            "(0020,000E) 'Series Instance UID', (0008,0018) 'SOP Instance "
-            "UID' and (0020,0052) 'Frame of Reference UID'"
+            "(0020,000E) 'Series Instance UID', (0008,0018) "
+            "'SOP Instance UID' and (0020,0052) 'Frame of Reference UID' "
         ),
     )
 
@@ -47,8 +48,9 @@ def setup_argparse():
     )
     parser.add_argument(
         '-nb',
-        action='store_false',
-        help="Don't create a backup of the original datasets"
+        action='store_true',
+        help="Don't create a backup of the original datasets",
+        default=False,
     )
     parser.add_argument(
         '--org-root',
@@ -59,6 +61,34 @@ def setup_argparse():
             "'1.2.826.0.1.3680043.8.498.')"
         ),
         default=PYDICOM_ROOT_UID,
+    )
+    parser.add_argument(
+        '--uid-length',
+        type=int,
+        help=(
+            "Limit the generated UID lengths to the specified number of "
+            "characters (maximum 64)"
+        ),
+        default=64,
+    )
+    parser.add_argument(
+        '--series',
+        action='store_true',
+        help=(
+            "Generate new UIDs at the Series level rather than the Study "
+            "level (i.e. only change (0020,000E) 'Series Instance UID' and "
+            "(0008,0018) 'SOP Instance UID')"
+        ),
+        default=False,
+    )
+    parser.add_argument(
+        '--force-for',
+        action='store_true',
+        help=(
+            "With --series, force a new (0020,0052) 'Frame of Reference UID' "
+            "value"
+        ),
+        default=False,
     )
 
     return parser
@@ -72,8 +102,14 @@ ALL_STUDY_UIDS = {}
 ALL_SERIES_UIDS = {}
 ALL_FOR_UIDS = {}
 
-if len(args.org_root) > 63:
-    sys.exit("The <org root> value must be less than 64 characters long")
+
+def _generate_uids(org_root, length):
+    """Return a generated pydicom UID with `length` characters."""
+    uid = UID(generate_uid(args.org_root)[:length])
+    if not uid.is_valid:
+        sys.exit("Unable to generate valid UIDs with the supplied arguments")
+
+    return uid
 
 
 # Recursively search the specified directory
@@ -90,35 +126,43 @@ for fpath in all_fpath:
         try:
             ds = dcmread(fpath, force=True)
 
-            if 'StudyInstanceUID' in ds:
+            if not args.series and 'StudyInstanceUID' in ds:
                 if ds.StudyInstanceUID not in ALL_STUDY_UIDS:
-                    ALL_STUDY_UIDS[ds.StudyInstanceUID] = generate_uid(args.org_root)
+                    ALL_STUDY_UIDS[ds.StudyInstanceUID] = _generate_uids(
+                        args.org_root, args.uid_length
+                    )
 
                 ds.StudyInstanceUID = ALL_STUDY_UIDS[ds.StudyInstanceUID]
 
             if 'SeriesInstanceUID' in ds:
                 if ds.SeriesInstanceUID not in ALL_SERIES_UIDS:
-                    ALL_SERIES_UIDS[ds.SeriesInstanceUID] = generate_uid(args.org_root)
+                    ALL_SERIES_UIDS[ds.SeriesInstanceUID] = _generate_uids(
+                        args.org_root, args.uid_length
+                    )
 
                 ds.SeriesInstanceUID = ALL_SERIES_UIDS[ds.SeriesInstanceUID]
 
-            if 'FrameOfReferenceUID' in ds:
+            # Frame of Reference may be related to one or more Series
+            #   so a new Study UID requires a new FoR UID (Part 3, A.1.2.5)
+            if (not args.series or args.force_for) and 'FrameOfReferenceUID' in ds:
                 if ds.FrameOfReferenceUID not in ALL_FOR_UIDS:
-                    ALL_FOR_UIDS[ds.FrameOfReferenceUID] = generate_uid(args.org_root)
+                    ALL_FOR_UIDS[ds.FrameOfReferenceUID] = _generate_uids(
+                        args.org_root, args.uid_length
+                    )
 
                 ds.FrameOfReferenceUID = ALL_FOR_UIDS[ds.FrameOfReferenceUID]
 
             if 'SOPInstanceUID' in ds:
-                ds.SOPInstanceUID = generate_uid(args.org_root)
+                ds.SOPInstanceUID = _generate_uids(args.org_root,
+                                                   args.uid_length)
 
                 if hasattr(ds, 'file_meta'):
                     ds.file_meta.MediaStorageSOPInstanceUID = ds.SOPInstanceUID
 
-            opath = fpath
-            if args.nb:
-                opath += '.orig'
+            if not args.nb:
+                shutil.copy(fpath, "{}.orig".format(fpath))
 
-            ds.save_as(opath, write_like_original=False)
+            ds.save_as(fpath, write_like_original=False)
         except Exception as exc:
             bad_files.append(fpath)
 
