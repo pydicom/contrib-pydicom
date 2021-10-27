3b06fa13b30e34b9e815583742cb2a22bc1b0ee9
AgnesMartinez <63485599+AgnesMartinez@users.noreply.github.com>
2021-10-26 19:37:53 -0500

    Fixed LUT method
    LUT is now applied with apply_modality_lut function.
    
    Added Image Brightness Control using ImageEnhance class from PIL
    
    Added create_GIF method to visualize DICOM files with multiple frames (videoclips)

--- viewers/pydicom_PIL.py
+++ viewers/pydicom_PIL.py
@@ -1,19 +1,17 @@
 # pydicom_PIL.py
 
 """View DICOM images using Python image Library (PIL)
-
 Usage:
 >>> import pydicom
 >>> from pydicom.contrib.pydicom_PIL import show_PIL
 >>> ds = pydicom.dcmread("filename")
 >>> show_PIL(ds)
-
 Requires Numpy:
     http://numpy.scipy.org/
-
-and Python Imaging Library:
+Python Imaging Library:
     http://www.pythonware.com/products/pil/
-
+and DICOM Modality LUT module
+    http://dicom.nema.org/medical/dicom/current/output/chtml/part03/sect_C.11.html#sect_C.11.1
 """
 # Copyright (c) 2009 Darcy Mason, Adit Panchal
 # This file is part of pydicom, relased under an MIT license.
@@ -25,36 +23,44 @@ and Python Imaging Library:
 # Tested on Python 2.5.4 (32-bit) on Mac OS X 10.6
 #    using numpy 1.3.0 and PIL 1.1.7b1
 
+
 try:
-    import PIL.Image
+    from PIL import Image,ImageEnhance
     have_PIL = True
 except ImportError:
     have_PIL = False
 
+#apply_modality_lut() function comes with pydicom module
 try:
-    import numpy as np
-    have_numpy = True
+    from pydicom.pixel_data_handlers.util import apply_modality_lut
+    have_lut = True
 except ImportError:
-    have_numpy = False
+    have_lut = False
 
 
-def get_LUT_value(data, window, level):
+def get_LUT_value(dataset):
     """Apply the RGB Look-Up Table for the given
-       data and window/level value."""
-    if not have_numpy:
-        raise ImportError("Numpy is not available."
-                          "See http://numpy.scipy.org/"
+       dataset"""
+    if not have_lut:
+        raise ImportError("pydicom is not available."
+                          "See https://pydicom.github.io/pydicom/dev/tutorials/installation.html"
                           "to download and install")
 
-    return np.piecewise(data,
-                        [data <= (level - 0.5 - (window - 1) / 2),
-                         data > (level - 0.5 + (window - 1) / 2)],
-                        [0, 255, lambda data: ((data - (level - 0.5)) /
-                         (window - 1) + 0.5) * (255 - 0)])
+    #Pixel Data
+    pixel_data = dataset.pixel_array
+
+    return apply_modality_lut(pixel_data,dataset)
 
 
-def get_PIL_image(dataset):
-    """Get Image object from Python Imaging Library(PIL)"""
+def get_PIL_image(dataset,brightness_factor = 1.0):
+    """Get Image object from Python Imaging Library(PIL)
+
+       Manipulate image brightness using brightness_factor parameter
+       Receives a float value
+       Default = 1.0
+       Brighter > 1.0 | Darker < 1.0 
+
+    """
     if not have_PIL:
         raise ImportError("Python Imaging Library is not available. "
                           "See http://www.pythonware.com/products/pil/ "
@@ -63,45 +69,149 @@ def get_PIL_image(dataset):
     if ('PixelData' not in dataset):
         raise TypeError("Cannot show image -- DICOM dataset does not have "
                         "pixel data")
-    # can only apply LUT if these window info exists
-    if ('WindowWidth' not in dataset) or ('WindowCenter' not in dataset):
-        bits = dataset.BitsAllocated
-        samples = dataset.SamplesPerPixel
-        if bits == 8 and samples == 1:
-            mode = "L"
-        elif bits == 8 and samples == 3:
-            mode = "RGB"
-        elif bits == 16:
-            # not sure about this -- PIL source says is 'experimental'
-            # and no documentation. Also, should bytes swap depending
-            # on endian of file and system??
-            mode = "I;16"
-        else:
-            raise TypeError("Don't know PIL mode for %d BitsAllocated "
-                            "and %d SamplesPerPixel" % (bits, samples))
+    
+    bits = dataset.BitsAllocated
 
-        # PIL size = (width, height)
-        size = (dataset.Columns, dataset.Rows)
+    samples = dataset.SamplesPerPixel
 
-        # Recommended to specify all details
-        # by http://www.pythonware.com/library/pil/handbook/image.htm
-        im = PIL.Image.frombuffer(mode, size, dataset.PixelData,
-                                  "raw", mode, 0, 1)
+    if bits == 8 and samples == 1:
 
+        mode = "L"
+
+    elif bits == 8 and samples == 3:
+
+        mode = "RGB"
+
+    elif bits == 16:
+        # not sure about this -- PIL source says is 'experimental'
+        # and no documentation. Also, should bytes swap depending
+        # on endian of file and system??
+        mode = "I;16"
     else:
-        ew = dataset['WindowWidth']
-        ec = dataset['WindowCenter']
-        ww = int(ew.value[0] if ew.VM > 1 else ew.value)
-        wc = int(ec.value[0] if ec.VM > 1 else ec.value)
-        image = get_LUT_value(dataset.pixel_array, ww, wc)
-        # Convert mode to L since LUT has only 256 values:
-        #   http://www.pythonware.com/library/pil/handbook/image.htm
-        im = PIL.Image.fromarray(image).convert('L')
+        
+        raise TypeError("Don't know PIL mode for %d BitsAllocated "
+                        "and %d SamplesPerPixel" % (bits, samples))
 
+    #LUTification returns ndarrays
+    #can only apply LUT if pydicom is installed
+    image = get_LUT_value(dataset)
+
+    try:
+        #if pixel data has only one frame
+        im = Image.fromarray(image).convert(mode)
+
+    except:
+        #When pixel data has multiple frames, output the first one
+        im = Image.fromarray(image[0]).convert(mode)
+
+    if not brightness_factor == 1.0:
+        #enhancer will enhance
+        enhancer = ImageEnhance.Brightness(im)
+
+        #save enhanced version
+        better_im = enhancer.enhance(brightness_factor)
+
+        return better_im
+
+    
+    #return default brightness
     return im
 
 
 def show_PIL(dataset):
     """Display an image using the Python Imaging Library (PIL)"""
     im = get_PIL_image(dataset)
+
     im.show()
+
+
+def create_GIF(dataset,brightness_factor = 1.0,filename="clip"):
+    """Create a GIF using the Python Imaging Library (PIL)
+    
+       Only if DICOM file has multiple frames
+
+       Manipulate image brightness using brightness_factor parameter,
+       receives a float value,
+       Default = 1.0
+       Brighter > 1.0 | Darker < 1.0
+
+       Use filename parameter to specify the name of the file created
+       Receives a string value
+    """
+    if not have_PIL:
+        raise ImportError("Python Imaging Library is not available. "
+                          "See http://www.pythonware.com/products/pil/ "
+                          "to download and install")
+
+    if ('PixelData' not in dataset):
+        raise TypeError("Cannot show image -- DICOM dataset does not have "
+                        "pixel data")
+    
+    bits = dataset.BitsAllocated
+
+    samples = dataset.SamplesPerPixel
+
+    if bits == 8 and samples == 1:
+
+        mode = "L"
+
+    elif bits == 8 and samples == 3:
+
+        mode = "RGB"
+
+    elif bits == 16:
+        # not sure about this -- PIL source says is 'experimental'
+        # and no documentation. Also, should bytes swap depending
+        # on endian of file and system??
+        mode = "I;16"
+    else:
+        
+        raise TypeError("Don't know PIL mode for %d BitsAllocated "
+                        "and %d SamplesPerPixel" % (bits, samples))
+
+    #Can only apply LUT if pydicom is installed                    
+    #LUTification returns ndarrays
+    frames = get_LUT_value(dataset)
+
+    #Create the first frame 
+    gif = Image.fromarray(frames[0]).convert(mode)
+
+    if not brightness_factor == 1.0:
+        #enhancer will enhance
+        enhancer = ImageEnhance.Brightness(gif)
+
+        #save enhanced version
+        better_gif = enhancer.enhance(brightness_factor)
+
+        #Create the rest of the frames
+        data = []
+
+        #Loop thru each frame
+        for item in frames[1:]:
+
+            frame = Image.fromarray(item).convert(mode)
+
+            enhancer = ImageEnhance.Brightness(frame)
+
+            better_frame = enhancer.enhance(brightness_factor)
+
+            data.append(better_frame)
+
+        better_gif.save(f'./{filename}.gif', save_all=True, quality=100 , append_images=data, optimize=True, duration=15, loop=0)
+    
+    #Create the rest of the frames
+    data = []
+
+    #Loop thru each frame
+    for item in frames[1:]:
+
+        frame = Image.fromarray(item).convert(mode)
+
+        enhancer = ImageEnhance.Brightness(frame)
+
+        better_frame = enhancer.enhance(brightness_factor)
+
+        data.append(better_frame)
+    
+    #default brightness
+    gif.save(f'./{filename}.gif', save_all=True, quality=100 , append_images=data, optimize=True, duration=15, loop=0)
